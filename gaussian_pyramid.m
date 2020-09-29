396e230097959d5eff872d09a8bedde5830ca348
evelynbankell <evelyn@bankell.se>
2020-09-29 11:55:36 +0200

    implement gaussian pyramid function

---
+++ gaussian_pyramid.m
@@ -0,0 +1,28 @@
+function [g_pyr] = gaussian_pyramid(I,level)
+
+% I - input image
+% level - number of levels in pyramid
+
+% allocate pyramid of n levels
+g_pyr = cell( level, 1 );
+
+% save input image in first level at the pyramid
+g_pyr{1} = I;
+
+filter = [1 4 6 4 1; 4 16 24 16 4; 6 24 36 24 6; 4 16 24 16 4; 1 4 6 4 1 ]/256;
+
+for n=2:level
+
+    % Lowpass filter
+    I = imfilter(I, filter, 'replicate');
+    
+    % Rezise image with 0.5
+    I = imresize(I,0.5);
+    
+    g_pyr{n} = I;
+
+end
+
+
+end
+
--- laplacian.m
+++ laplacian.m
@@ -5,8 +5,15 @@ I = im2double(I);
 %w = [1 4 6 4 1; 4 16 24 16 4; 6 24 36 24 6; 4 16 24 16 4; 1 4 6 4 1 ]/256;
 a = ML(I);
 b = SML(a);
-plot(b)
+%plot(b)
+
+%figure
+%imshow(a, [])
+%%imshow(Is)
+
+gauss = gaussian_pyramid(I, 5);
 
 figure
-imshow(a, [])
-%%imshow(Is)
+imshow(gauss{1}, [])
+figure
+imshow(gauss{5}, [])
